c96bb67c4e5d1f831284ea53cd6c1fac57125753
Aleksandr Noskov <nemestny@gmail.com>
2018-06-02 20:54:21 +0500

    Task 2 short version

--- task2.rb
+++ task2.rb
@@ -1,10 +1,5 @@
-n = ARGV[0].to_i
-i = 0
-pos = n.positive? ? 1 : -1
+n = ARGV[0].to_i.abs
+pos = ARGV[0].to_i.positive? ? 1 : -1
 fib_hash = Hash.new
-loop do
-	fib_hash[i] = (-1..1).include?(i)? i : fib_hash[i-1*pos] + fib_hash[i-2*pos]
-	break if i == n
-	i+=pos
-end
-puts fib_hash[n]
+0.upto(n) {|i| fib_hash[i] = i < 2 ? i : fib_hash[i-1] + fib_hash[i-2] }
+puts fib_hash[n]*pos
