f3a8cc456649a83d26e5a6807ba89c1938527411
Aleksandr Noskov <nemestny@gmail.com>
2018-06-02 20:39:18 +0500

    Task 3 no wrong spaces

--- task3.rb
+++ task3.rb
@@ -1,4 +1,4 @@
 vowels = ARGV[0].scan(/[AaEeIiOoUu]/)
 digits = ARGV[0].scan(/\d/)
 consonant = ARGV[0].scan(/\w/) - vowels - digits
-puts vowels.join + ' ' + consonant.join + ' ' + digits.join
+puts vowels.join + (consonant.length > 0 ? ' ' + consonant.join : '') + (digits.length > 0 ? ' ' + digits.join : '')
