30a0f56d5839fa46de1ba3dcf4f912aaa593f028
Aleksandr Noskov <nemestny@gmail.com>
2018-06-04 08:55:57 +0500

    Task 3 prepare for wrong string input

--- task3.rb
+++ task3.rb
@@ -1,4 +1,5 @@
-vowels = ARGV[0].scan(/[AaEeIiOoUu]/)
-digits = ARGV[0].scan(/\d/)
-consonant = ARGV[0].scan(/\w/) - vowels - digits
+str = ARGV.join
+vowels = str.scan(/[AaEeIiOoUu]/)
+digits = str.scan(/\d/)
+consonant = str.scan(/\w/) - vowels - digits
 puts vowels.join + (consonant.length > 0 ? ' ' + consonant.join : '') + (digits.length > 0 ? ' ' + digits.join : '')
