e7a2466da471f260996f9ae2db4f7aa159e776d1
Aleksandr Noskov <nemestny@gmail.com>
2018-06-05 23:13:33 +0500

    Revert "Merge branch 'task_2'"
    This reverts commit 43b0027f95a827bd1e97590c425a8c2b8e3c8a7d, reversing
    changes made to db65d9debec1757972ed7337c91bc3daebebb7ad.

--- task2.rb
+++ task2.rb
@@ -1,4 +1,5 @@
 n = ARGV[0].to_i.abs
+pos = ARGV[0].to_i.positive? ? 1 : -1
 fib_array = [0,1]
 2.upto(n) {|i| fib_array[i] = fib_array[i-1] + fib_array[i-2] }
-puts ARGV[0].to_i.positive? ? fib_array[n] : -fib_array[n]
+puts fib_array[n]*pos
