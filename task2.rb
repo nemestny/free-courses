57a28bd7995b22dc8c75940a1e6b7feee81e55da
Aleksandr Noskov <nemestny@gmail.com>
2018-06-05 12:25:30 +0500

    Task 2 remove pos var

--- task2.rb
+++ task2.rb
@@ -1,5 +1,4 @@
 n = ARGV[0].to_i.abs
-pos = ARGV[0].to_i.positive? ? 1 : -1
 fib_array = [0,1]
 2.upto(n) {|i| fib_array[i] = fib_array[i-1] + fib_array[i-2] }
-puts fib_array[n]*pos
+puts ARGV[0].to_i.positive? ? fib_array[n] : -fib_array[n]
