4a63ba5fbb2255fdde525c8a86b4d12ffbb585c4
Aleksandr Noskov <nemestny@gmail.com>
2018-06-06 08:49:48 +0500

    Task 2 Never Believe Wikipedia

--- task2.rb
+++ task2.rb
@@ -1,4 +1,4 @@
 n = ARGV[0].to_i.abs
 fib_array = [0,1]
 2.upto(n) {|i| fib_array[i] = fib_array[i-1] + fib_array[i-2] }
-puts ARGV[0].to_i.positive? ? fib_array[n] : -fib_array[n]
+puts ARGV[0].to_i.positive?||n.odd? ? fib_array[n] : -fib_array[n]
