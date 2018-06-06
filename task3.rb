61d99abb87f26f71cacd12dfd4efb032b1a63fa8
Aleksandr Noskov <nemestny@gmail.com>
2018-06-06 14:22:53 +0500

    Task 3 Minor update

--- task3.rb
+++ task3.rb
@@ -1,7 +1,7 @@
 str = ARGV[0].gsub(/\W/,'')
 
 a = str.scan(/[aeiou]/i).join
-b = str.scan(/[^aeiou\d\W]/i).join
+b = str.scan(/[^aeiou\d]/i).join
 c = str.scan(/\d/).join
 
 puts ("#{a}" + (b.empty? ? '': ' ') + "#{b} #{c}").strip
