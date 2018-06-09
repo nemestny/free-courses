dd87ce3c9ead60d07c6ec126659b5d3698e39b53
Aleksandr Noskov <nemestny@gmail.com>
2018-06-09 10:22:51 +0500

    Task 3 One monster line, no spaces control

--- task3.rb
+++ task3.rb
@@ -1,4 +1,1 @@
-a = ARGV[0].scan(/[aeiou]/i).join
-b = ARGV[0].scan(/[^aeiou\d\W]/i).join
-c = ARGV[0].scan(/\d/).join
-puts ("#{a}" + (b.empty? ? '': ' ') + "#{b} #{c}").strip
+puts ("#{ARGV[0].scan(/[aeiou]/i).join} #{ARGV[0].scan(/[^aeiou\d\W]/i).join} #{ARGV[0].scan(/\d/).join}").strip
