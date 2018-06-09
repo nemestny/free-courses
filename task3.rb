c132f01cd74a59d6ad965f7cd1e42bfde089ad2e
Aleksandr Noskov <nemestny@gmail.com>
2018-06-09 09:08:15 +0500

    Task 3 Remove space lines

--- task3.rb
+++ task3.rb
@@ -1,7 +1,5 @@
-str = ARGV[0].gsub(/\W/,'')
-
+str = ARGV[0].scan(/\w/).join
 a = str.scan(/[aeiou]/i).join
 b = str.scan(/[^aeiou\d]/i).join
 c = str.scan(/\d/).join
-
 puts ("#{a}" + (b.empty? ? '': ' ') + "#{b} #{c}").strip
