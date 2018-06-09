8d32da02221faf8614a3e7218724c30a2fba1dba
Aleksandr Noskov <nemestny@gmail.com>
2018-06-09 09:55:36 +0500

    Task 2 Perfect recursive!!!

--- task2.rb
+++ task2.rb
@@ -1,7 +1,13 @@
-def fibonacci n
-	pos = n.positive? ? 1 : -1
-	@fib_hash[n] = (-1..1).include?(n)? n.abs : pos*fibonacci(n-1*pos) + fibonacci(n-2*pos) if !@fib_hash[n]
-	@fib_hash[n]
+def fibonacci(n,fib_hash = Hash.new)
+	if fib_hash[n]
+		fib_hash[n]
+	elsif (-1..1).include?(n)
+		fib_hash[n] = n.abs
+	elsif n.positive?
+		fib_hash[n] = fibonacci(n-2,fib_hash) + fibonacci(n-1,fib_hash)
+	else
+		fib_hash[n] = fibonacci(n+2,fib_hash) - fibonacci(n+1,fib_hash)	
+	end
 end
-@fib_hash = Hash.new
+
 puts fibonacci(ARGV[0].to_i)
