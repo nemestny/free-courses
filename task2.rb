1225237919d7f61516facd9941835d7ea406bcc9
Aleksandr Noskov <nemestny@gmail.com>
2018-06-12 19:29:24 +0500

    Tasks 2,3 - all comment

--- task2.rb
+++ task2.rb
@@ -1,13 +1,13 @@
-def fibonacci(n,fib_hash = Hash.new)
-	if fib_hash[n]
-		fib_hash[n]
-	elsif (-1..1).include?(n)
-		fib_hash[n] = n.abs
-	elsif n.positive?
-		fib_hash[n] = fibonacci(n-2,fib_hash) + fibonacci(n-1,fib_hash)
-	else
-		fib_hash[n] = fibonacci(n+2,fib_hash) - fibonacci(n+1,fib_hash)	
-	end
-end
+# def fibonacci(n,fib_hash = Hash.new)
+# 	if fib_hash[n]
+# 		fib_hash[n]
+# 	elsif (-1..1).include?(n)
+# 		fib_hash[n] = n.abs
+# 	elsif n.positive?
+# 		fib_hash[n] = fibonacci(n-2,fib_hash) + fibonacci(n-1,fib_hash)
+# 	else
+# 		fib_hash[n] = fibonacci(n+2,fib_hash) - fibonacci(n+1,fib_hash)	
+# 	end
+# end
 
-puts fibonacci(ARGV[0].to_i)
+# puts fibonacci(ARGV[0].to_i)
