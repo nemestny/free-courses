0e33be1df7056d1b8338b105c52352faf667ae45
Aleksandr Noskov <nemestny@gmail.com>
2018-06-13 15:43:29 +0500

    Revert "Tasks 2,3 - all comment"
    This reverts commit 1225237919d7f61516facd9941835d7ea406bcc9.

--- task3.rb
+++ task3.rb
@@ -1,1 +1,1 @@
-# puts (ARGV[0].scan(/[aeiou]/i)+[' ']+ARGV[0].scan(/[^aeiou\d\W]/i)+[' ']+ARGV[0].scan(/\d/)).join.strip
+puts (ARGV[0].scan(/[aeiou]/i)+[' ']+ARGV[0].scan(/[^aeiou\d\W]/i)+[' ']+ARGV[0].scan(/\d/)).join.strip
