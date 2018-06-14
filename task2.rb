372e76fa8862d78b54caf7d6f7e2ab1a91e2f631
Aleksandr Noskov <nemestny@gmail.com>
2018-06-14 08:23:23 +0500

    Task 2 Edit hash create

--- task2.rb
+++ task2.rb
@@ -1,8 +1,6 @@
-def f(n,f_hash = Hash.new)
+def f(n,f_hash = {-1 => 1, 0 => 0, 1 => 1})
 	if f_hash[n]
 		f_hash[n]
-	elsif (-1..1).include?(n)
-		f_hash[n] = n.abs
 	elsif n.negative?
 		f_hash[n] = f(n+2,f_hash) - f(n+1,f_hash)
 	else
