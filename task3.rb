c317d7581b9eb312592e40437827aadd195e28ef
Aleksandr Noskov <nemestny@gmail.com>
2018-06-14 08:15:37 +0500

    Task 3 Y is vowels?

--- task3.rb
+++ task3.rb
@@ -1,4 +1,4 @@
-a = ARGV[0].scan(/[aeiou]/i).join
-b = ARGV[0].scan(/[^aeiou\d\W]/i).join
+a = ARGV[0].scan(/[aeiouy]/i).join
+b = ARGV[0].scan(/[^aeiouy\d\W]/i).join
 c = ARGV[0].scan(/\d/).join
 puts ("#{a}" + (b.empty? ? '': ' ') + "#{b} #{c}").strip
