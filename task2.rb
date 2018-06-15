c7faffda3212ca299be2ee9a01d8624a31c68bb5
Aleksandr Noskov <nemestny@gmail.com>
2018-06-15 14:48:03 +0500

    Task 2 Very simple version no hash

--- task2.rb
+++ task2.rb
@@ -1,18 +1,9 @@
-def f_recursive n, f_hash
-	pos = n.positive? ? 1 : -1
-	f_hash[n] ? f_hash[n] : f_hash[n] = f_recursive(n-2*pos,f_hash) + pos*f_recursive(n-1*pos,f_hash)
-end
-
-def f_bine n
-	phi = (1 + 5**0.5)/2
-	(phi**n-(-phi)**(-n))/(2*phi - 1)
-end
-
-def f_test n
+def f n
 	f0 = 0
 	f1 = 1
 	f2 = 1
 	fi = 0
+
 	if n.negative?
 		-1.downto(n) do |i|
 			fi = f1 - f0
@@ -26,11 +17,8 @@ def f_test n
 			f2 = fi
 		end
 	end
+	
 	fi
 end
 
-puts f_recursive(ARGV[0].to_i,{0 => 0, 1 => 1, 2 => 1})
-
-puts f_bine(ARGV[0].to_i).to_i
-
-puts f_test(ARGV[0].to_i)
+puts f(ARGV[0].to_i)
