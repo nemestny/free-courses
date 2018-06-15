48469c9606f4834ac57553acf6afdf861dc03246
Aleksandr Noskov <nemestny@gmail.com>
2018-06-15 13:33:40 +0500

    Task 2 ultra very short version recursive. What more do you need ?!

--- task2.rb
+++ task2.rb
@@ -1,6 +1,5 @@
 def f n, f_hash
 	pos = n.positive? ? 1 : -1
-	f_hash[n] = f(n-2*pos,f_hash) + pos*f(n-1*pos,f_hash) if !f_hash[n]
-	f_hash[n]
+	f_hash[n] ? f_hash[n] : f_hash[n] = f(n-2*pos,f_hash) + pos*f(n-1*pos,f_hash)
 end
 puts f(ARGV[0].to_i,{0 => 0, 1 => 1, 2 => 1})
