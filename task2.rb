8a24644d323a545cd333b0b629f401106aca08d2
Aleksandr Noskov <nemestny@gmail.com>
2018-06-21 10:26:05 +0500

    Task 2 correct view

--- task2.rb
+++ task2.rb
@@ -1,22 +1,17 @@
 def f n
-	f0 = 0
-	f1 = 1
-	f2 = 1
-	fi = 0
+	f0,f1,f2,fi = 0,1,1,0
 
 	if (1..2).include?(n)
 		fi = 1
 	elsif n.negative?
 		-1.downto(n) do |i|
 			fi = f1 - f0
-			f1 = f0
-			f0 = fi
+			f1,f0 = f0,fi
 		end
 	else
 		3.upto(n) do |i|
 			fi = f2 + f1
-			f1 = f2
-			f2 = fi
+			f1,f2 = f2,fi
 		end
 	end
 	fi
